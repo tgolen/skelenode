67c845f7686ac6b96edcb2f685806fa1d6175407
Tim Golen <tgolen@appcelerator.com>
2015-02-27 14:19:11 -0500

    cleaning up files
    Signed-off-by: Tim Golen <tgolen@appcelerator.com>

--- config/default.js
+++ config/default.js
@@ -1,13 +1,10 @@
 'use strict';
 
-var sh = require('execSync'),
-	hash = sh.exec('git rev-parse HEAD').stdout || '';
-
 module.exports = {
 	host: 'localhost',
 	port: 80,
 	env: process.env.NODE_ENV || 'development',
-	githash: hash.substring(0,7),
+	githash: '',
 	db: {
 		redis: {
 			port: 6379,
