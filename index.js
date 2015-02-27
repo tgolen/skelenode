d0f852f3ce7099eb82ea2d51164c059a9b840602
Tim Golen <tgolen@appcelerator.com>
2015-02-27 13:43:04 -0500

    make the server startable and configurable
    Signed-off-by: Tim Golen <tgolen@appcelerator.com>

--- index.js
+++ index.js
@@ -13,59 +13,75 @@ var chalk = require('chalk'),
 	skelenodeModelLoader = require('skelenode-model-loader'),
 	skelenodeSocketXHR = require('skelenode-socket-xhr');
 
-// create our server
-var app = restify.createServer({
-	log: bunyan.createLogger({
-		name: packageJSON.name,
-		stream: process.stdout
-	})
-});
+exports.start = start;
 
-// add appropriate middleware
-app.use(restify.bodyParser());
-app.use(morgan('dev'));
+function start(config) {
+	// create our server
+	var app = restify.createServer({
+			log: bunyan.createLogger({
+				name: packageJSON.name,
+				stream: process.stdout
+			})
+		}),
+		options = config || {};
 
-// setup our socket XHR handler
-skelenodeSocketXHR(app, config.get('db.redis.port'), config.get('db.redis.host'), config.get('db.redis.password'), config.get('dispatcher.debug'));
+	// setup some of our config options
+	if (!options.pathToViews) {
+		options.pathToViews = '../app/views';
+	}
+	if (!options.pathToModels) {
+		options.pathToModels = __dirname + '../app/models';
+	}
+	if (!options.pathToPublic) {
+		options.pathToPublic = __dirname + '../public';
+	}
 
-// turn on an audit logger if we need to
-if (config.get('logger.audit.enabled')) {
-	app.on('after', restify.auditLogger({
-		log: bunyan.createLogger({
-			name: packageJSON.name + '_audit',
-			stream: process.stdout
-		})
+	// add appropriate middleware
+	app.use(restify.bodyParser());
+	app.use(morgan('dev'));
+
+	// setup our socket XHR handler
+	skelenodeSocketXHR(app, config.get('db.redis.port'), config.get('db.redis.host'), config.get('db.redis.password'), config.get('dispatcher.debug'));
+
+	// turn on an audit logger if we need to
+	if (config.get('logger.audit.enabled')) {
+		app.on('after', restify.auditLogger({
+			log: bunyan.createLogger({
+				name: packageJSON.name + '_audit',
+				stream: process.stdout
+			})
+		}));
+	}
+
+	// serve our index file on root
+	app.get('/', require(options.pathToViews + '/index'));
+
+	// replace the githash in URLs so that the githash doesn't
+	// have to be an actual directory
+	app.use(function(req, res, next) {
+		req.url = req.url.replace('/'+config.get('githash')+'/', '');
+		next();
+	});
+
+	// server our static files via our githash so they are version
+	// controlled
+	var githashRegex = new RegExp('\/'+config.get('githash')+'\/.*');
+	app.get(githashRegex, restify.serveStatic({
+		directory: options.pathToPublic
 	}));
+
+	// configure swagger
+	swagger.setAppHandler(app);
+	swagger.configure('http://petstore.swagger.wordnik.com', '0.1');
+	swagger.configureSwaggerPaths('', '/api-docs', '');
+
+	// load all of our models
+	skelenodeModelLoader.init(options.pathToModels);
+
+	// startup our server
+	app.listen(config.get('port'));
+	console.log(chalk.cyan('[%s] Application started on port %s in %s environment.'),
+		packageJSON.name.toUpperCase(),
+		chalk.underline(config.get('port')),
+		chalk.underline(config.util.getEnv('NODE_ENV')));
 }
-
-// serve our index file on root
-app.get('/', require('./app/views/index'));
-
-// replace the githash in URLs so that the githash doesn't
-// have to be an actual directory
-app.use(function(req, res, next) {
-	req.url = req.url.replace('/'+config.get('githash')+'/', '');
-	next();
-});
-
-// server our static files via our githash so they are version
-// controlled
-var githashRegex = new RegExp('\/'+config.get('githash')+'\/.*');
-app.get(githashRegex, restify.serveStatic({
-	directory: __dirname + '/public'
-}));
-
-// configure swagger
-swagger.setAppHandler(app);
-swagger.configure('http://petstore.swagger.wordnik.com', '0.1');
-swagger.configureSwaggerPaths('', '/api-docs', '');
-
-// load all of our models
-skelenodeModelLoader.init(__dirname + '/app/models');
-
-// startup our server
-app.listen(config.get('port'));
-console.log(chalk.cyan('[%s] Application started on port %s in %s environment.'),
-	packageJSON.name.toUpperCase(),
-	chalk.underline(config.get('port')),
-	chalk.underline(config.util.getEnv('NODE_ENV')));
